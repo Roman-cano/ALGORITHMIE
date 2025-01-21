baef978e7f8287dfe613b52f8d8cf2200b683478
RomanBonaparte <roman.cano04@gmail.com>
2025-01-21 12:21:15 +0100

    les boucles 2

--- bases/src/Boucles.java
+++ bases/src/Boucles.java
@@ -34,12 +34,84 @@ public class Boucles {
         // carré de nombre
         // ////////////////
 
-        int nombre = 10;
+        int nombre = 5;
         for (int i = 0; i < nombre; i++) {
             for (int j = 0; j < nombre; j++) {
-                System.out.print("j");
+                System.out.print(i + " ");
             }
             System.out.println("");
         }
+
+
+        System.out.println("------------------------------");
+
+        // /////////////////
+        // triangle de nombre
+        // ////////////////
+        int nb = 5;
+        for (int i = 0; i < nb; i++) {
+            for (int y = 0; y < i; y++) {
+                System.out.print(i + " ");
+            }
+            System.out.println("");
+
+        }
+
+        // /////////////////
+        // triangle de nombre avec espace
+        // ////////////////
+
+        int nb2 = 5;
+        for (int i = 0; i < nb; i++) {
+
+            for (int space = 0; space <= nb2 - i +2 -1; space++) {
+                System.out.print(" ");
+            }
+            for (int y = 0; y < i; y++) {
+                System.out.print(i + " ");
+            }
+            System.out.println("");
+
+        }
+
+        System.out.println("------------------------------");
+
+
+        // /////////////////
+        // calcul factoriel
+        // ////////////////
+
+        int fact = 5;
+        int resultat = 1;
+        for (int i = 1; i <= fact; i++) {
+            resultat *= i;
+        }
+        System.out.println("Le factoriel de " + fact + " est: " + resultat);
+
+        System.out.println("------------------------------");
+
+        // /////////////////
+        // palindrome
+        // ////////////////
+
+        String mot = "kayak";
+        boolean estPalindrome = true;
+        for (int i = 0; i < mot.length() / 2; i++) {
+            if (mot.charAt(i) != mot.charAt(mot.length() - i - 1)) {
+                estPalindrome = false;
+                break;
+            }
+        }
+        if (estPalindrome) {
+            System.out.println(mot + " est un palindrome");
+        } else {
+            System.out.println(mot + " n'est pas un palindrome");
+        }
+
+        System.out.println("------------------------------");
     }
+
+    // /////////////////
+    // Boucle while
+    // ////////////////
 }
